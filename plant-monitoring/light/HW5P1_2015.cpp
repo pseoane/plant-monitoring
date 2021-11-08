1befca4a62bf3520a614bccd52df70dd90c77d5b
Paulo Seoane Davila <paulo.seoane@udc.es>
2021-11-08 01:15:56 +0100

    added metrics manager for advanced mode

--- plant-monitoring/light/HW5P1_2015.cpp
+++ plant-monitoring/light/HW5P1_2015.cpp
@@ -1,8 +1,14 @@
 #include "./HW5P1_2015.h"
+#include "./MetricsManager.h"
 
-HW5P1_2015::HW5P1_2015(AnalogIn ain) : input(ain) {};
+HW5P1_2015::HW5P1_2015(AnalogIn ain) : input(ain) {
+	metricsManager = MetricsManager();
+};
+
 HW5P1_2015::~HW5P1_2015() {};
 
 float HW5P1_2015::readLight() {
-	return float(input.read_u16()) / float(UINT16_MAX) * 100.0;
+	float value = float(input.read_u16()) / float(UINT16_MAX) * 100.0;
+	metricsManager.addValue(value);
+	return value;
 }	
--- plant-monitoring/main.cpp
+++ plant-monitoring/main.cpp
@@ -14,31 +14,43 @@
 
 #define TEMP_LIMIT_MIN  0
 #define TEMP_LIMIT_MAX	45
+#define TEMP_ALERT_COLOR "RED"
 
 #define HUM_LIMIT_MIN   10
 #define HUM_LIMIT_MAX		95
+#define HUM_ALERT_COLOR "GREEN"
 
 #define SOILM_LIMIT_MIN 10
 #define SOILM_LIMIT_MAX 90
+#define SOILM_ALERT_COLOR "BLUE"
 
 #define LIGHT_LIMIT_MIN 2
 #define LIGHT_LIMIT_MAX 95
+#define LIGHT_ALERT_COLOR "REDGREEN"
 
 #define GREEN_LIMIT_MIN 1 //450
+#define NOT_GREEN_ALERT_COLOR "REDBLUE"
+
 #define STAND_LIMIT			30
+#define NOT_STAND_ALERT_COLOR "REDGREENBLUE"
+
 
 #define CLEAR_MIN_LIMIT 1 //if below, clear led = ON
 
-#define NORMAL_MODE_CADENCE 2500ms //Seconds of normal mode cadence monitoring without scaling
+#define NORMAL_MODE_CADENCE 3000ms
+#define TEST_MODE_CADENCE 2000ms
+#define COMPUTE_METRICS_CADENCE 15000ms //1 hour in miliseconds
 
 #define N_MEASURES 			10   //Number of measures made in NORMAL_MODE after which mean,min,max values are displayed without scaling
 
 #define SCALE_FACTOR    12  // With this factor we can scale to fullfill th requirements (avg,min,.max,... each 1h) 
 
+using namespace std::chrono;
 
 enum Mode { TEST, NORMAL };
 
-Mode currentMode = TEST;
+Mode currentMode;
+
 MMA8451Q acc(PB_9,PB_8,0x1d<<1);
 TCS3472_I2C rgbSensor(PB_9, PB_8);
 HW5P1_2015 lightSensor(A0);
@@ -47,33 +59,33 @@ Si7021 humtempsensor(PB_9,PB_8);
 SEN_13322 soilMoistureSensor(PA_0);
 UnbufferedSerial* gps_Serial = new UnbufferedSerial(PA_9, PA_10,9600); //serial object for use w/ GPS
 Adafruit_GPS myGPS(gps_Serial); //object of Adafruit's GPS class
-Ticker ticker;
-bool tick_event;
-void ticker_isr(void){tick_event = true;}
-char const* colorNames[3] = {"RED", "GREEN", "BLUE"};
-using namespace std::chrono;
-bool buttonPressed = false;
-float accValues[3];
-uint16_t rgbValues[4];
 InterruptIn userButton(PB_2);
 Thread gps_thread(osPriorityNormal,2048);;
-bool gpsInfoAvailable = false;
+
 uint8_t hour, minute, seconds_gps, year, month, day;
 uint16_t milliseconds_gps;
+uint8_t fixquality, satellites;
 float latitude, longitude, geoidheight, altitude;
 float speed, angle;
 char lat, lon, mag;
-bool fix;
-uint8_t fixquality, satellites;
-uint8_t nmeasuresdone= 0;
-Mutex mutex;
+char const* colorNames[3] = {"RED", "GREEN", "BLUE"};
+bool tick_event;
+bool gpsInfoAvailable = false;
+bool buttonPressed = false;
+bool shouldComputeMetrics = false;
 
+Mutex mutex;
+Ticker ticker;
+
+void ticker_isr(void){
+	tick_event = true;
+}
 
 void buttonPressedIsr() {
 	buttonPressed = true;
 }
 
-void readGps(void) {
+void readGps() {
 	char c; //when read via Adafruit_GPS::read(), the class returns single character stored here
 	//Timer refresh_Timer; //sets up a timer for use in loop; how often do we print GPS info?
 	//const int refresh_Time = 2000; //refresh time in ms
@@ -110,79 +122,124 @@ void readGps(void) {
 		}
 }
 
-void normalMode() {
-	printf("Norrrmal\n");
-	ticker.attach(ticker_isr, NORMAL_MODE_CADENCE );
-	while (!buttonPressed){
-		if(tick_event){
-			nmeasuresdone ++;
-			if(nmeasuresdone == N_MEASURES){
-				//print media,min,max,...
-				nmeasuresdone = 0;
-			}
-			
-			
-			
-			
-			
-			if (gpsInfoAvailable){
-				mutex.lock();
-				printf("Time: %d:%d:%d.%u\r\n", hour, minute, seconds_gps, milliseconds_gps);
-				printf("Date: %d/%d/20%d\r\n",day, month, year);
-				printf("Quality: %d\r\n", (int) fixquality);
-				//if ((int)myGPS.fixquality > 0) {
-				printf("Location: %5.2f %c, %5.2f %c\r\n", latitude, lat, longitude, lon);
-				printf("Speed: %5.2f knots\r\n", speed);
-				printf("Angle: %5.2f\r\n", angle);
-				printf("Altitude: %5.2f\r\n", altitude);
-				printf("Satellites: %d\r\n", satellites);
-				mutex.unlock();
-				gpsInfoAvailable = false;
-			}
-			tick_event = false;
-	
-		}
-	}
+void printValues(float acc[], uint16_t rgb[], char const* dominantColorName, float light, float humidity, float temp, float soilMoisture) {
+	printf("ACCELEROMETER: X_AXIS=%f \t Y_AXIS=%f\t Z_AXIS=%f\n", acc[0], acc[1], acc[2]);
+	printf("COLOR SENSOR: CLEAR=%d, RED=%d, GREEN=%d, BLUE=%d -- DOMINANT COLOR=%s\n", rgb[0], rgb[1], rgb[2], rgb[3], dominantColorName);
+	printf("LIGHT: %3.1f%%", light);
+	printf("TEMPERATURE: %2.2f C \n", temp);
+	printf("HUMIDITY: %2.2f%%  \n", humidity);
+	printf("SOIL MOISTURE: %2.2f%% \n", soilMoisture);
+	printf("\n\n");
 }
-void testMode(){
-	printf("Testing\n");
-	ticker.attach(ticker_isr, 2000ms);
-	while(!buttonPressed) {
-		if(tick_event){
+
+void printGpsInfo() {
+	mutex.lock();
+	printf("Time: %d:%d:%d.%u\r\n", hour, minute, seconds_gps, milliseconds_gps);
+	printf("Date: %d/%d/20%d\r\n",day, month, year);
+	printf("Quality: %d\r\n", (int) fixquality);
+	//if ((int)myGPS.fixquality > 0) {
+	printf("Location: %5.2f %c, %5.2f %c\r\n", latitude, lat, longitude, lon);
+	printf("Speed: %5.2f knots\r\n", speed);
+	printf("Angle: %5.2f\r\n", angle);
+	printf("Altitude: %5.2f\r\n", altitude);
+	printf("Satellites: %d\r\n", satellites);
+	mutex.unlock();
+	gpsInfoAvailable = false;
+}
+
+void computeMetricsTickerIsr() {
+	shouldComputeMetrics = true;
+}
+
+void printMetrics() {
+	printf("METRICS FOR LIGHT IN THE LAST HOUR\n");
+	printf("AVG %3.1f\n", lightSensor.metricsManager.computeAverage());
+	printf("MAX %3.1f\n", lightSensor.metricsManager.computeMax());
+	printf("MIN %3.1f\n", lightSensor.metricsManager.computeMin());
+	
+	printf("METRICS FOR TEMPERATURE IN THE LAST HOUR\n");
+	printf("AVG %3.1f\n", humtempsensor.tempMetricsManager.computeAverage());
+	printf("MAX %3.1f\n", humtempsensor.tempMetricsManager.computeMax());
+	printf("MIN %3.1f\n", humtempsensor.tempMetricsManager.computeMin());
+	
+	printf("METRICS FOR HUMIDITY IN THE LAST HOUR\n");
+	printf("AVG %3.1f\n", humtempsensor.humMetricsManager.computeAverage());
+	printf("MAX %3.1f\n", humtempsensor.humMetricsManager.computeMax());
+	printf("MIN %3.1f\n", humtempsensor.humMetricsManager.computeMin());	
+	
+	printf("METRICS FOR SOIL MOISTURE IN THE LAST HOUR\n");
+	printf("AVG %3.1f\n", soilMoistureSensor.metricsManager.computeAverage());
+	printf("MAX %3.1f\n", soilMoistureSensor.metricsManager.computeMax());
+	printf("MIN %3.1f\n", soilMoistureSensor.metricsManager.computeMin());	
+	
+	printf("\n\n");
+	// Do this for all the other sensors
+}
+
+void normalMode() {
+	ticker.attach(ticker_isr, NORMAL_MODE_CADENCE);
+	Ticker measuresTicker;
+	measuresTicker.attach(computeMetricsTickerIsr, COMPUTE_METRICS_CADENCE);
+	
+	rgbLed.setColor(0, 0, 0);
+	while (!buttonPressed){
+		if(tick_event){			
+			float accValues[3];
+			uint16_t rgbValues[4];
 			acc.getAllAxis(accValues);
 			humtempsensor.measure();
 			int dominantColor = rgbSensor.getAllColors(rgbValues); // 0 = RED, 1 = GREEN, 2 = BLUE
 			char const* dominantColorName = colorNames[dominantColor];
-			rgbLed.setColor(dominantColor);
-			printf("ACCELEROMETER: X_AXIS=%f \t Y_AXIS=%f\t Z_AXIS=%f\n", accValues[0], accValues[1], accValues[2]);
-			printf("COLOR SENSOR: CLEAR=%d, RED=%d, GREEN=%d, BLUE=%d -- DOMINANT COLOR=%s\n", rgbValues[0], rgbValues[1], rgbValues[2], rgbValues[3], dominantColorName);
-			printf("LIGHT: %3.1f%%", lightSensor.readLight());
-			printf("TEMPERATURE: %2.2f C \n ", humtempsensor.get_temperature());
-			printf("HUMIDITY: %2.2f%%  \n ", humtempsensor.get_humidity());
-			printf("SOIL MOISTURE: %2.2f%% \n ", soilMoistureSensor.getMoistureValue());
-			printf("\n\n");
-			if (gpsInfoAvailable){
-				printf("Time: %d:%d:%d.%u\r\n", hour, minute, seconds_gps, milliseconds_gps);
-				printf("Date: %d/%d/20%d\r\n", day, month, year);
-				printf("Quality: %d\r\n", (int) fixquality);
-				//if ((int)myGPS.fixquality > 0) {
-				printf("Location: %5.2f %c, %5.2f %c\r\n", latitude, lat, longitude, lon);
-				printf("Speed: %5.2f knots\r\n",speed);
-				printf("Angle: %5.2f\r\n", angle);
-				printf("Altitude: %5.2f\r\n", altitude);
-				printf("Satellites: %d\r\n", satellites);
-				gpsInfoAvailable = false;
+			float humidity = humtempsensor.get_humidity();
+			float temp = humtempsensor.get_temperature();
+			float soilMoisture = soilMoistureSensor.getMoistureValue();
+			float light  = lightSensor.readLight();
+			printValues(accValues, rgbValues, dominantColorName, light, humidity, temp, soilMoisture);
 			
+			if (gpsInfoAvailable){
+				// printGpsInfo();
+			}
+			
+			if (shouldComputeMetrics) {
+				printMetrics();
+				// Also update a global variable for the predominant color
+				lightSensor.metricsManager.reset();
+				shouldComputeMetrics = false;
+			}
+			
+			// Check alerts here
+			
+			tick_event = false;
+		}
+	}
+}
+
+void testMode() {
+	ticker.attach(ticker_isr, TEST_MODE_CADENCE);
+	while(!buttonPressed) {
+		if(tick_event){
+			float accValues[3];
+			uint16_t rgbValues[4];
+			acc.getAllAxis(accValues);
+			humtempsensor.measure();
+			int dominantColor = rgbSensor.getAllColors(rgbValues); // 0 = RED, 1 = GREEN, 2 = BLUE
+			char const* dominantColorName = colorNames[dominantColor];
+			float humidity = humtempsensor.get_humidity();
+			float temp = humtempsensor.get_temperature();
+			float soilMoisture = soilMoistureSensor.getMoistureValue();
+			float light  = lightSensor.readLight();
+			rgbLed.setColor(dominantColor);
+			printValues(accValues, rgbValues, dominantColorName, light, humidity, temp, soilMoisture);
+			if (gpsInfoAvailable){
+				//printGpsInfo();
 			}
 			tick_event = false;
 		}
 	}
-	
-
 }
 
 
-int main(void){
+int main(void) {
 	rgbSensor.enablePowerAndRGBC();
 
 	//GPS
@@ -190,7 +247,7 @@ int main(void){
 	userButton.fall(buttonPressedIsr);
 	currentMode = TEST;
 	testMode();
-	while(true){
+	while(true) {
 		if (buttonPressed) {
 			switch (currentMode) {
 				case(TEST):
@@ -206,10 +263,6 @@ int main(void){
 					testMode();
 					break;
 			}
-			
 		}
-		
 	}
 }	
-
-
