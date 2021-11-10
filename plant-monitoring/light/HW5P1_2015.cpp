3cdb83c24563fcc54a834808bf52f6c9fb8d13af
carlosclemente <carlos.clemente.99@gmail.com>
2021-11-10 19:12:31 +0100

    light sensor calibrated

--- plant-monitoring/light/HW5P1_2015.cpp
+++ plant-monitoring/light/HW5P1_2015.cpp
@@ -8,7 +8,8 @@ HW5P1_2015::HW5P1_2015(AnalogIn ain) : input(ain) {
 HW5P1_2015::~HW5P1_2015() {};
 
 float HW5P1_2015::readLight() {
-	float value = float(input.read_u16()) / float(UINT16_MAX) * 100.0;
+	float value = float(input.read_u16()) / float(UINT16_MAX) * 300.0;
+	value = value > 100 ? 100 : value;
 	metricsManager.addValue(value);
 	return value;
 }	
